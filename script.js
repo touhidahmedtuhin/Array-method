5853378a543e1a2bf204784c900f4b199ac8dd56
touhidahmedtuhin <tuhintouhidahmed@gmail.com>
2025-07-16 23:32:12 +0600

    array method

--- script.js
+++ script.js
@@ -39,3 +39,10 @@
 // })
 // console.log(newArr);
 //...............................................................
+let n = prompt('enter a number');
+let arr = [];
+
+for(let i =1 ; i <=n ; i++){
+    arr[i-1] = i;
+}
+console.log(arr);
