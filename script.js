07d5901639c21c0adb2930b36ca627ab6588b027
touhidahmedtuhin <tuhintouhidahmed@gmail.com>
2025-07-16 23:42:50 +0600

    array method

--- script.js
+++ script.js
@@ -39,10 +39,14 @@
 // })
 // console.log(newArr);
 //...............................................................
-// let n = prompt('enter a number');
-// let arr = [];
+let n = prompt('enter a number');
+let arr = [];
+for(let i =1 ; i <=n ; i++){
+    arr[i-1] = i; // 1(i-1) = 1, 2(i-1) = 2, 3(i-1) = 3 
+}
+console.log(arr);
 
-// for(let i =1 ; i <=n ; i++){
-//     arr[i-1] = i; // 1(i-1) = 1, 2(i-1) = 2, 3(i-1) = 3 
-// }
-// console.log(arr);
+let newAr = arr.reduce((prev , curr) => {
+  return prev + curr;
+})
+console.log(`Sum of all numbers is: ${newAr}`);
