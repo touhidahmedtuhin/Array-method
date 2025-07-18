5704527ebf834a1bee260fa22007d6c419c511d4
Touhid Ahmed Tuhin <tuhin2668@gmail.com>
2025-07-18 20:39:18 +0600

    Update script.js
    Remove some comment

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
-// let arr = ['Dhaka','Tangail','Chittagong','Sylhet','Khulna','Rajshahi','Barisal','Rangpur','Mymensingh'];
-
-// arr.forEach((city) => {
-//     console.log(city);
-// })
+let arr = ['Dhaka','Tangail','Chittagong','Sylhet','Khulna','Rajshahi','Barisal','Rangpur','Mymensingh'];
+arr.forEach((city) => {
+    console.log(city);
+})
 //...............................................................
 //forEach Method
-// let arr = [2, 4, 6, 8, 10];
-// arr.forEach((num) => {
-//    console.log(num * num);
-// })
+let arr = [2, 4, 6, 8, 10];
+arr.forEach((num) => {
+   console.log(num * num);
+})
 //...............................................................
 // Callback Function 
-// let arr = [1, 2, 3, 4, 5];
-// let calSqt = (num) => {
-//   console.log(num * num);
-// }
-// arr.forEach(calSqt);
+let arr = [1, 2, 3, 4, 5];
+let calSqt = (num) => {
+  console.log(num * num);
+}
+arr.forEach(calSqt);
 //...............................................................
 // Map Method
-// let arr = [1, 2, 3, 4, 5];
-// let newArr = arr.map((num) => {
-//   return num * num;
-// })
-// console.log(newArr);
+let arr = [1, 2, 3, 4, 5];
+let newArr = arr.map((num) => {
+  return num * num;
+})
+console.log(newArr);
 //...............................................................
 // Filter Method
-// let arr = [ 2,5,8,10,12,14,16,18,20];
-// let newArr = arr.filter((val) => {
-//   return val % 2 === 0;
-// })
-// console.log(newArr);
+let arr = [ 2,5,8,10,12,14,16,18,20];
+let newArr = arr.filter((val) => {
+  return val % 2 === 0;
+})
+console.log(newArr);
 //...............................................................
 // Reduce Method
-// let arr = [12, 3, 5, 7, 9, 11, 13, 15, 17, 9];
+let arr = [12, 3, 5, 7, 9, 11, 13, 15, 17, 9];
 
-// let newArr = arr.reduce((previousValue, currentValue) => {
-//    return previousValue > currentValue ? previousValue : currentValue;
-// })
-// console.log(newArr);
+let newArr = arr.reduce((previousValue, currentValue) => {
+   return previousValue > currentValue ? previousValue : currentValue;
+})
+console.log(newArr);
 //...............................................................
 // Sum of all numbers from 1 to n using reduce method
 let n = prompt('enter a number');
